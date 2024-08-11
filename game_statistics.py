cc7a490c2815d2f3788ff22b56ed6633eb12d3f8
krishna <krishnakarki1996@gmail.com>
2024-08-12 02:44:37 +0545

    change highscore if score greater, prepare high score image

--- game_statistics.py
+++ game_statistics.py
@@ -4,8 +4,14 @@ class GameStats:
         """initialize statistics of the game."""
         self.settings = ai_game.Settings
         self.reset_stats()
+        self.high_score = 0
 
     def reset_stats(self):
         """initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
         self.score = 0
+
+    def check_high_score(self):
+        """check if the current score is greater than high score"""
+        if self.score > self.high_score:
+            self.high_score = self.score
--- scoreboard.py
+++ scoreboard.py
@@ -13,8 +13,9 @@ class ScoreBoard:
         self.font_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
 
-        # prepare the initial scores image
+        # prepare the initial score, and high score image
         self.prep_score()
+        self.prep_high_score()
 
     def prep_score(self):
         """prepare the score image to display on the window."""
@@ -32,3 +33,18 @@ class ScoreBoard:
     def draw_score(self):
         """draw the score image to its defined rect."""
         self.screen.blit(self.score_img, self.score_img_rect)
+
+    def prep_high_score(self):
+        """prepare the high score image and
+        set its position respective to the window."""
+        # round the high score to the nearest 10 and give commas to score
+        self.stats.high_score = round(self.stats.high_score, -1)
+        high_score = f"High_Score: {self.stats.high_score:,}"
+
+        self.highscore_img = self.font.render(high_score, True, self.font_color)
+
+        # position the high score at the middle of the window
+        self.highscore_img_rect = self.highscore_img.get_rect()
+        self.highscore_img_rect.centerx = self.screen_rect.centerx
+        self.highscore_img_rect.top = self.score_img_rect.top
+
