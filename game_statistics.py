93b7950b0afb65f64d68a74d21992447958aa5b7
krishna <krishnakarki1996@gmail.com>
2024-08-14 14:09:18 +0545

    create a highscore file to store highscore

--- game_statistics.py
+++ game_statistics.py
@@ -4,7 +4,7 @@ class GameStats:
         """initialize statistics of the game."""
         self.settings = ai_game.Settings
         self.reset_stats()
-        self.high_score = 0
+        self._get_high_score()
 
     def reset_stats(self):
         """initialize statistics that can change during the game."""
@@ -13,6 +13,14 @@ class GameStats:
         self.level = 1
 
     def check_high_score(self):
-        """check if the current score is greater than high score"""
+        """check if the current score is greater than high score,
+        and write it in highscore file."""
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("highscore.txt", "w") as file:
+                file.write(str(self.high_score))
+
+    def _get_high_score(self):
+        """get the high score from file."""
+        with open("highscore.txt", "r") as file:
+            self.high_score = int(file.read())
